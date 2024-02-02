8d8d56595e55c6b257784bb5d9ad54c4040d1f6e
mramzannasir <mramzannasir555@gmail.com>
2024-02-03 01:12:37 +0500

    final

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,23 +1,29 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import classNames from "classnames";
 import { AiFillCaretUp } from "react-icons/ai";
+import "./style.css";
+
 const DonationTabs = () => {
   const [activeMainTab, setActiveMainTab] = useState(0);
   const [activeSubTabMonthly, setActiveSubTabMonthly] = useState(0);
   const [activeSubTabOnce, setActiveSubTabOnce] = useState(0);
+  const [selectedAmount, setSelectedAmount] = useState("");
 
   const handleMainTabClick = (index) => {
     setActiveMainTab(index);
     setActiveSubTabMonthly(0);
     setActiveSubTabOnce(0);
+    setSelectedAmount(""); // Clear selected amount when changing tabs
   };
 
   const handleSubTabMonthlyClick = (index) => {
     setActiveSubTabMonthly(index);
+    setSelectedAmount(monthlyMoneyQuantities[index]);
   };
 
   const handleSubTabOnceClick = (index) => {
     setActiveSubTabOnce(index);
+    setSelectedAmount(onceMoneyQuantities[index]);
   };
 
   const monthlyMoneyQuantities = [
@@ -39,112 +45,80 @@ const DonationTabs = () => {
 
   return (
     <>
-      <div className="flex items-center gap-2 mt-16 font-medium">
+      <div className="tabs-container">
         <div
           onClick={() => handleMainTabClick(0)}
-          className={classNames(
-            "py-2 px-4 duration-300 cursor-pointer bg-white border-t-2 border-l-2 border-r-2 border-gray-300",
-            {
-              "active-tab-parent border-white border-t-2 border-l-2 border-r-2":
-                activeMainTab === 0,
-            }
-          )}
+          className={classNames("tab-parent", {
+            "active-tab-parent": activeMainTab === 0,
+          })}
         >
           Donate Monthly
         </div>
         <div
           onClick={() => handleMainTabClick(1)}
-          className={classNames(
-            "py-2 px-4 duration-300 cursor-pointer bg-white border-t-2 border-l-2 border-r-2 border-gray-300",
-            {
-              "active-tab-parent border-white border-t-2 border-l-2 border-r-2":
-                activeMainTab === 1,
-            }
-          )}
+          className={classNames("tab-parent", {
+            "active-tab-parent": activeMainTab === 1,
+          })}
         >
           Donate Once
         </div>
       </div>
-      <div className="w-[] border-gray-300  border-[1px] p-5">
-        <h1 className="my-1 font-medium">I would like to donation of</h1>
-        <div className="">
-          {activeMainTab === 0 && (
-            <div>
-              <div className="grid md:flex grid-cols-2 md:justify-between md:grid-cols-6 gap-4 font-semibold mt-4">
-                {monthlyMoneyQuantities.map((quantity, index) => (
-                  <div
-                    key={index}
-                    onClick={() => handleSubTabMonthlyClick(index)}
-                    className={classNames(
-                      "py-2 px-5 cursor-pointer border mx-1 bg-[#eaedf4] border-gray-500",
-                      {
-                        "active-tab border border-[#009ca0] text-white":
-                          activeSubTabMonthly === index,
-                        "bg-[#fe9235] !border-[#fe9235]":
-                          index === monthlyMoneyQuantities.length - 1,
-                      }
-                    )}
-                  >
-                    {index === monthlyMoneyQuantities.length - 1
-                      ? ""
-                      : "Donate £"}
-                    {quantity}
-                  </div>
-                ))}
+      <div className="tab-content">
+        <h1 className="my-1 font-medium">I would like to donate</h1>
+        <div className="donation-options">
+          {activeMainTab === 0 &&
+            monthlyMoneyQuantities.map((quantity, index) => (
+              <div
+                key={index}
+                onClick={() => handleSubTabMonthlyClick(index)}
+                className={classNames("donation-option", {
+                  "active-tab": activeSubTabMonthly === index,
+                  "last-child": index === monthlyMoneyQuantities.length - 1,
+                })}
+              >
+                {index === monthlyMoneyQuantities.length - 1
+                  ? selectedAmount
+                    ? `£${selectedAmount}`
+                    : "Other Amount"
+                  : `£${quantity}`}
               </div>
-              <div className="mt-4 bg-[#ffce9f] p-2 relative">
-                <p>
-                  Donate £ 40 could help answer an emergency call to our Animal
-                  rescue line
-                </p>
-                <div className="absolute top-[-20px] text-[#ffce9f] right-16">
-                  <AiFillCaretUp size={30} />
-                </div>
+            ))}
+          {activeMainTab === 1 &&
+            onceMoneyQuantities.map((quantity, index) => (
+              <div
+                key={index}
+                onClick={() => handleSubTabOnceClick(index)}
+                className={classNames("donation-option", {
+                  "active-tab": activeSubTabOnce === index,
+                  "last-child": index === onceMoneyQuantities.length - 1,
+                })}
+              >
+                {index === onceMoneyQuantities.length - 1
+                  ? selectedAmount
+                    ? `£${selectedAmount}`
+                    : "Other Amount"
+                  : `£${quantity}`}
               </div>
-            </div>
-          )}
-          {activeMainTab === 1 && (
-            <div>
-              <div className="grid md:flex grid-cols-2 md:justify-between md:grid-cols-6 gap-4  mt-4 font-semibold">
-                {onceMoneyQuantities.map((quantity, index) => (
-                  <div
-                    key={index}
-                    onClick={() => handleSubTabOnceClick(index)}
-                    className={classNames(
-                      "py-2 px-5 cursor-pointer border mx-1 bg-[#eaedf4] border-gray-500",
-                      {
-                        "active-tab border border-[#009ca0] text-white":
-                          activeSubTabOnce === index,
-                        "bg-[#fe9235] !border-[#fe9235]":
-                          index === onceMoneyQuantities.length - 1,
-                      }
-                    )}
-                  >
-                    {index === onceMoneyQuantities.length - 1 ? "" : "Donate £"}
-                    {quantity}
-                  </div>
-                ))}
-              </div>
-              <div className="mt-4 bg-[#ffce9f] p-2 relative">
-                <p>
-                  £ 12 could help answer an emergency call to our Animal rescue
-                  line
-                </p>
-                <div className="absolute top-[-20px] text-[#ffce9f] right-16">
-                  <AiFillCaretUp size={30} />
-                </div>
-              </div>
-            </div>
-          )}
+            ))}
+        </div>
+        <div className="donation-info">
+          <p>
+            {activeMainTab === 0
+              ? `Donate £40 could help answer an emergency call to our Animal rescue line`
+              : `£12 could help answer an emergency call to our Animal rescue line`}
+          </p>
+          <div className="caret-up">
+            <AiFillCaretUp size={30} />
+          </div>
         </div>
       </div>
-      <div className="w-full bg-[#eaedf4] flex justify-between items-center px-6 shadow-md h-[80px]">
-        <div className="underline font-medium">i would like to pay</div>
-        <div className="flex gap-3 items-center">
-          <img className="w-[40px]" src="/image.png" alt="" />
-          <img src="/mastercard-logo.png" alt="" />
-          <img src="/paypal-logo.png" alt="" />
-          <img src="/fund.png" alt="" />
+      <div className="payment-options">
+        <div className="underline font-medium">I would like to pay</div>
+        <div className="payment-methods">
+          <img className="logo" src="/image.png" alt="" />
+          <img className="logo" src="/mastercard-logo.png" alt="" />
+          <img className="logo" src="/paypal-logo.png" alt="" />
+          <img className="logo" src="/fund.png" alt="" />
         </div>
       </div>
     </>
